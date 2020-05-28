d7ea7eb1fa5ed1b38dfeb3e1d10785e9af1bba35
robiulbuet <61079032+robiulbuet@users.noreply.github.com>
2020-05-28 12:50:08 +0600

    update

--- Assignment4/SpeakGoodBye.js
+++ Assignment4/SpeakGoodBye.js
@@ -1,5 +1,5 @@
 (function(window){
-	var byeSpeaker = new object();
+	var byeSpeaker = new Object();
 	var speakWord = "Good Bye";
 	byeSpeaker.speak=function speak(name){
 		console.log(speakWord + " " + name);
--- Assignment4/SpeakHello.js
+++ Assignment4/SpeakHello.js
@@ -1,5 +1,5 @@
 (function(window) {
-	var helloSpeaker = new object();
+	var helloSpeaker = new Object();
 	var speakWord = "Hello";
 	helloSpeaker.speak=function speak(name){
 		console.log(speakWord + " " + name);
